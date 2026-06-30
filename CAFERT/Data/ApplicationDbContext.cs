710d1bb901296c0935f70abb63cb1a1269c4b9be
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-06-30 18:26:46 +0300

    admin design assets added

---
+++ CAFERT/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CAFERT.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
